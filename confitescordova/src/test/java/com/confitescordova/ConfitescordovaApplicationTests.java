c06dd444fe1e0762e53df1c2248728a9187bf93a
Diego Valdes <diego.valdes.f@usach.cl>
2024-10-09 10:46:44 -0300

    proyecto iniciado

---
+++ confitescordova/src/test/java/com/confitescordova/ConfitescordovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.confitescordova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfitescordovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
