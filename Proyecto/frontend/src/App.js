0fcc4212f90ebe81d3d4fc260bc2a433f563f238
valenpy22 <valentina.campos.o@usach.cl>
2024-11-19 17:53:56 -0300

    feat: sidebar y eliminación de vistas innecesarias

--- Proyecto/frontend/src/App.js
+++ Proyecto/frontend/src/App.js
@@ -1,40 +1,20 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import ProductSlider from './components/Slider';
-import ProductList from './components/ProductList';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import AddProductPage from './pages/AddProductPage';
-import OrderPage from './pages/OrderPage';
-import AdminEditProductPage from './pages/AdminEditProductPage'; 
 import './App.css';
-import AdminOrdersPage from './pages/AdminOrdersPage';
+import OrdersPage from './pages/OrdersPage';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
+        <Sidebar />
         <div className="content">
           <Routes>
-            <Route path="/" element={
-              <>
-                <ProductSlider />
-                <ProductList />
-              </>
-            } />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/add-product" element={<AddProductPage />} />
-            <Route path="/order" element={<OrderPage />} />
-            <Route path="/admin/edit-product" element={<AdminEditProductPage />} /> {/* Ruta para edición de productos */}
-            <Route path="/admin/orders" element={<AdminOrdersPage />}/>
+            <Route path="/admin/orders" element={<OrdersPage />}/>
           </Routes>
         </div>
-        <Footer />
       </div>
     </Router>
   );
--- Proyecto/frontend/tailwind.config.js
+++ Proyecto/frontend/tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
             "pink-pastel": "#F7A1C4",
             "cyan-pastel": "#78D5D7",
             "yellow-pastel": "#FFE787",
+            purple: {
+                800: '#4a3f6e',
+                600: '#6c5b9b',
+              },
         }
     },
   },
