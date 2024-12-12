a38b098196a68291288c26defc21bd066e24bd10
valenpy22 <valentina.campos.o@usach.cl>
2024-12-11 23:14:59 -0300

    delete: componentes no usados eliminados en frontend

--- Proyecto/frontend/src/App.js
+++ Proyecto/frontend/src/App.js
@@ -2,15 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { GlobalProvider } from "./utils/GlobalModelContext"; // Importa el proveedor
 import Sidebar from "./components/Sidebar";
-import HomePage from "./pages/HomePage";
 import OrdersPage from "./pages/OrdersPage";
-import MenuPage from "./pages/MenuPage";
-import CostsPage from "./pages/CostsPage";
 import AddOrderPage from "./pages/AddOrderPage";
 import Dashboard from "./pages/Dashboard";
 import ExcelPage from "./pages/ExcelPage";
 
-import New_Statistics from "./pages/New_Statistics"
+import Statistics from "./pages/Statistics";
 
 const App = () => {
   return (
@@ -20,14 +17,11 @@ const App = () => {
           <Sidebar />
           <div className="flex-1 p-6">
             <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/menu" element={<MenuPage />} />
               <Route path="/orders" element={<OrdersPage />} />
-              <Route path="/costs" element={<CostsPage />} />
               <Route path="/statistics" element={<Dashboard />} />
               <Route path="/add-order" element={<AddOrderPage />} />
               <Route path="/excel" element={<ExcelPage />}/>
-              <Route path="/new_statistics" element={<New_Statistics />} />
+              <Route path="/statistics" element={<Statistics />} />
             </Routes>
           </div>
         </div>
